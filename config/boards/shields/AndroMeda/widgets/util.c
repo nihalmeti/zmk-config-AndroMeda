1ef650405aee8162474b2186eb39876a7b0671f6
nihalmeti <nihalgmeti@gmail.com>
2025-01-04 05:15:52 +0530

    animations

--- config/boards/shields/AndroMeda/widgets/util.c
+++ config/boards/shields/AndroMeda/widgets/util.c
@@ -1,46 +1,33 @@
-/*
- *
- * Copyright (c) 2023 The ZMK Contributors
- * SPDX-License-Identifier: MIT
- *
- */
-
 #include <zephyr/kernel.h>
 #include "util.h"
+#include <ctype.h>
 
-LV_IMG_DECLARE(bolt);
+void to_uppercase(char *str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        str[i] = toupper(str[i]);
+    }
+}
 
 void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]) {
-    static lv_color_t cbuf_tmp[CANVAS_SIZE * CANVAS_SIZE];
+    static lv_color_t cbuf_tmp[BUFFER_SIZE * BUFFER_SIZE];
     memcpy(cbuf_tmp, cbuf, sizeof(cbuf_tmp));
+
     lv_img_dsc_t img;
     img.data = (void *)cbuf_tmp;
     img.header.cf = LV_IMG_CF_TRUE_COLOR;
-    img.header.w = CANVAS_SIZE;
-    img.header.h = CANVAS_SIZE;
+    img.header.w = BUFFER_SIZE;
+    img.header.h = BUFFER_SIZE;
 
     lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);
-    lv_canvas_transform(canvas, &img, 900, LV_IMG_ZOOM_NONE, -1, 0, CANVAS_SIZE / 2,
-                        CANVAS_SIZE / 2, true);
+    lv_canvas_transform(canvas, &img, 900, LV_IMG_ZOOM_NONE, -1, 0, BUFFER_SIZE / 2,
+                        BUFFER_SIZE / 2, false);
 }
 
-void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
+void fill_background(lv_obj_t *canvas) {
     lv_draw_rect_dsc_t rect_black_dsc;
     init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
-    lv_draw_rect_dsc_t rect_white_dsc;
-    init_rect_dsc(&rect_white_dsc, LVGL_FOREGROUND);
 
-    lv_canvas_draw_rect(canvas, 0, 2, 29, 12, &rect_white_dsc);
-    lv_canvas_draw_rect(canvas, 1, 3, 27, 10, &rect_black_dsc);
-    lv_canvas_draw_rect(canvas, 2, 4, (state->battery + 2) / 4, 8, &rect_white_dsc);
-    lv_canvas_draw_rect(canvas, 30, 5, 3, 6, &rect_white_dsc);
-    lv_canvas_draw_rect(canvas, 31, 6, 1, 4, &rect_black_dsc);
-
-    if (state->charging) {
-        lv_draw_img_dsc_t img_dsc;
-        lv_draw_img_dsc_init(&img_dsc);
-        lv_canvas_draw_img(canvas, 9, -1, &bolt, &img_dsc);
-    }
+    lv_canvas_draw_rect(canvas, 0, 0, BUFFER_SIZE, BUFFER_SIZE, &rect_black_dsc);
 }
 
 void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
@@ -60,10 +47,4 @@ void init_line_dsc(lv_draw_line_dsc_t *line_dsc, lv_color_t color, uint8_t width
     lv_draw_line_dsc_init(line_dsc);
     line_dsc->color = color;
     line_dsc->width = width;
-}
-
-void init_arc_dsc(lv_draw_arc_dsc_t *arc_dsc, lv_color_t color, uint8_t width) {
-    lv_draw_arc_dsc_init(arc_dsc);
-    arc_dsc->color = color;
-    arc_dsc->width = width;
 }
--- config/boards/shields/AndroMeda/widgets/util.h
+++ config/boards/shields/AndroMeda/widgets/util.h
@@ -1,14 +1,14 @@
-/*
- *
- * Copyright (c) 2023 The ZMK Contributors
- * SPDX-License-Identifier: MIT
- *
- */
+#pragma once
 
 #include <lvgl.h>
 #include <zmk/endpoints.h>
 
-#define CANVAS_SIZE 68
+#define SCREEN_WIDTH 68
+#define SCREEN_HEIGHT 160
+
+#define BUFFER_SIZE 68
+#define BUFFER_OFFSET_MIDDLE -44
+#define BUFFER_OFFSET_BOTTOM -129
 
 #define LVGL_BACKGROUND                                                                            \
     IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_INVERTED) ? lv_color_black() : lv_color_white()
@@ -31,17 +31,10 @@ struct status_state {
 #endif
 };
 
-struct battery_status_state {
-    uint8_t level;
-#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-    bool usb_present;
-#endif
-};
-
+void to_uppercase(char *str);
 void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]);
-void draw_battery(lv_obj_t *canvas, const struct status_state *state);
-void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
-                    lv_text_align_t align);
+void fill_background(lv_obj_t *canvas);
 void init_rect_dsc(lv_draw_rect_dsc_t *rect_dsc, lv_color_t bg_color);
 void init_line_dsc(lv_draw_line_dsc_t *line_dsc, lv_color_t color, uint8_t width);
-void init_arc_dsc(lv_draw_arc_dsc_t *arc_dsc, lv_color_t color, uint8_t width);
+void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
+                    lv_text_align_t align);
