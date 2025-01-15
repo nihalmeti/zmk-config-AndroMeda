dbd2a5b0c74958f99033462fc00d25c72ed9300c
nihalmeti <nihalgmeti@gmail.com>
2025-01-15 16:33:57 +0530

    caps lock led

--- config/boards/shields/AndroMeda/src/led.c
+++ config/boards/shields/AndroMeda/src/led.c
@@ -12,7 +12,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #define LED_GPIO_NODE_ID DT_COMPAT_GET_ANY_STATUS_OKAY(gpio_leds)
 
 BUILD_ASSERT(DT_NODE_EXISTS(DT_ALIAS(indicator_led)),
-             "An alias for the Caps Lock LED is not found for LED_INDICATOR");
+             "An alias for the indicator LED is not found for LED_INDICATOR");
 
 static const struct device *led_dev = DEVICE_DT_GET(LED_GPIO_NODE_ID);
 static const uint8_t led_idx = DT_NODE_CHILD_IDX(DT_ALIAS(indicator_led));
@@ -20,7 +20,7 @@ static const uint8_t led_idx = DT_NODE_CHILD_IDX(DT_ALIAS(indicator_led));
 static bool initialized = false;
 
 #if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
-static int caps_lock_listener_cb(const zmk_event_t *eh) {
+static int led_hid_indicator_listener_cb(const zmk_event_t *eh) {
     if (!initialized) {
         return 0;
     }
@@ -28,31 +28,28 @@ static int caps_lock_listener_cb(const zmk_event_t *eh) {
     zmk_hid_indicators_t indicators = as_zmk_hid_indicators_changed(eh)->indicators;
 
     if (indicators & HID_USAGE_LED_CAPS_LOCK) {
-        LOG_INF("Caps Lock ON: Turning LED on");
+        LOG_INF("Caps ON: Turning LED ON");
         led_on(led_dev, led_idx);
     } else {
-        LOG_INF("Caps Lock OFF: Turning LED off");
+        LOG_INF("Caps OFF: Turning LED OFF");
         led_off(led_dev, led_idx);
     }
 
     return 0;
 }
 
-ZMK_LISTENER(caps_lock_listener, caps_lock_listener_cb);
-ZMK_SUBSCRIPTION(caps_lock_listener, zmk_hid_indicators_changed);
+ZMK_LISTENER(led_hid_indicator_listener, led_hid_indicator_listener_cb);
+ZMK_SUBSCRIPTION(led_hid_indicator_listener, zmk_hid_indicators_changed);
 #endif // IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
 
-static int led_init(const struct device *dev) {
-    ARG_UNUSED(dev);
-
+void led_init(void) {
     if (!device_is_ready(led_dev)) {
-        LOG_ERR("LED device not ready");
-        return -ENODEV;
+        LOG_ERR("LED device is not ready");
+        return;
     }
 
-    LOG_INF("Caps Lock LED initialized");
+    LOG_INF("LED device initialized");
     initialized = true;
-    return 0;
 }
 
 SYS_INIT(led_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
