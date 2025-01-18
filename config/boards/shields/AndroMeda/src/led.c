8aed39cfb0bdce154d82dc8149286ac9e62ed8c0
nihalmeti <nihalgmeti@gmail.com>
2025-01-19 01:16:03 +0530

    led caps indicator

--- config/boards/shields/AndroMeda/src/led.c
+++ config/boards/shields/AndroMeda/src/led.c
@@ -1,7 +1,6 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/led.h>
-#include <zephyr/init.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
@@ -17,39 +16,22 @@ BUILD_ASSERT(DT_NODE_EXISTS(DT_ALIAS(indicator_led)),
 static const struct device *led_dev = DEVICE_DT_GET(LED_GPIO_NODE_ID);
 static const uint8_t led_idx = DT_NODE_CHILD_IDX(DT_ALIAS(indicator_led));
 
-static bool initialized = false;
-
 #if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
-static int led_hid_indicator_listener_cb(const zmk_event_t *eh) {
-    if (!initialized) {
-        return 0;
-    }
 
-    zmk_hid_indicators_t indicators = as_zmk_hid_indicators_changed(eh)->indicators;
-
-    if (indicators & HID_USAGE_LED_CAPS_LOCK) {
-        LOG_INF("Caps ON: Turning LED ON");
+static int led_capslock_listener_cb(const zmk_event_t *eh) {
+    zmk_led_indicators_flags_t flags = zmk_led_indicators_get_current_flags();
+    
+    if (flags & ZMK_LED_INDICATORS_CAPSLOCK_BIT) {
+        // Turn the LED on when Caps Lock is active
         led_on(led_dev, led_idx);
     } else {
-        LOG_INF("Caps OFF: Turning LED OFF");
+        // Turn the LED off when Caps Lock is inactive
         led_off(led_dev, led_idx);
     }
-
     return 0;
 }
 
-ZMK_LISTENER(led_hid_indicator_listener, led_hid_indicator_listener_cb);
-ZMK_SUBSCRIPTION(led_hid_indicator_listener, zmk_hid_indicators_changed);
+ZMK_LISTENER(led_indicators_listener, led_capslock_listener_cb);
+ZMK_SUBSCRIPTION(led_indicators_listener, zmk_led_indicators_changed);
+
 #endif // IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
-
-void led_init(void) {
-    if (!device_is_ready(led_dev)) {
-        LOG_ERR("LED device is not ready");
-        return;
-    }
-
-    LOG_INF("LED device initialized");
-    initialized = true;
-}
-
-SYS_INIT(led_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
