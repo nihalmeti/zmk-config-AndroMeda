35fe5776954ca6794fc1fc89b554c1a791ecee6e
nihalmeti <nihalgmeti@gmail.com>
2025-01-19 02:39:35 +0530

    led caps indicator

--- config/boards/shields/AndroMeda/src/led.c
+++ config/boards/shields/AndroMeda/src/led.c
@@ -1,37 +1,53 @@
-#include <zmk/events/hid_indicators_changed.h>
 #include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/led.h>
+#include <zephyr/kernel.h>
+#include <zephyr/init.h>
+#include <zephyr/bluetooth/services/bas.h>
+
+#include <zmk/ble.h>
+#include <zmk/endpoints.h>
+#include <zmk/events/ble_active_profile_changed.h>
+#include <zmk/split/bluetooth/peripheral.h>
+#include <zmk/events/split_peripheral_status_changed.h>
+#include <zmk/events/battery_state_changed.h>
+
+#include <zmk/led_indicators.h>
+#include <zmk/events/led_indicator_changed.h>
+
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-// Define the device for your LED
-static const struct device *led_dev = DEVICE_DT_GET(DT_ALIAS(indicator_led));
+#define LED_GPIO_NODE_ID DT_COMPAT_GET_ANY_STATUS_OKAY(gpio_leds)
 
-// Callback for handling HID indicator changes
-static int hid_capslock_listener_cb(const zmk_event_t *eh) {
-    const struct zmk_hid_indicators_changed *ev = as_zmk_hid_indicators_changed(eh);
-    if (ev == NULL) {
-        return -ENOTSUP;
-    }
+// GPIO-based LED device and indices of red/green/blue LEDs inside its DT node
+static const struct device *led_dev = DEVICE_DT_GET(LED_GPIO_NODE_ID);
 
-    if (ev->indicators & HID_USAGE_LED_CAPS_LOCK) {
-        // Turn on the LED
-        if (led_dev != NULL && device_is_ready(led_dev)) {
-            gpio_pin_set(led_dev, DT_GPIO_PIN(DT_ALIAS(indicator_led), gpios), 1);
-        } else {
-            LOG_ERR("LED device not ready");
-        }
+static int led_capslock_listener_cb(const zmk_event_t *eh) {
+    zmk_led_indicators_flags_t flags = zmk_led_indicators_get_current_flags();
+
+    if (flags & ZMK_LED_INDICATORS_CAPSLOCK_BIT) {
+        led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_main)));
+
     } else {
-        // Turn off the LED
-        if (led_dev != NULL && device_is_ready(led_dev)) {
-            gpio_pin_set(led_dev, DT_GPIO_PIN(DT_ALIAS(indicator_led), gpios), 0);
-        } else {
-            LOG_ERR("LED device not ready");
-        }
+        led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_main)));
     }
+
     return 0;
 }
 
-// Register the listener
-ZMK_LISTENER(hid_indicators_listener, hid_capslock_listener_cb);
-ZMK_SUBSCRIPTION(hid_indicators_listener, zmk_hid_indicators_changed);
+ZMK_LISTENER(led_indicators_listener, led_capslock_listener_cb);
+ZMK_SUBSCRIPTION(led_indicators_listener, zmk_led_indicators_changed);
+
+static int leds_init(const struct device *device) {
+    if (!device_is_ready(led_dev)) {
+        LOG_ERR("Device %s is not ready", led_dev->name);
+        return -ENODEV;
+    }
+
+    return 0;
+}
+
+// run leds_init on boot
+SYS_INIT(leds_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
